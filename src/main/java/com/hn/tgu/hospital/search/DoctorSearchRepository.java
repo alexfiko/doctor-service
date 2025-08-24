2f8383b485c6dd7f5fcc344425c9c2096885e5df
alexfiko <alex.espinal.hn@gmail.com>
2025-08-23 22:07:21 -0600

    Update DoctorSearchRepository.java

--- src/main/java/com/hn/tgu/hospital/search/DoctorSearchRepository.java
+++ src/main/java/com/hn/tgu/hospital/search/DoctorSearchRepository.java
@@ -11,7 +11,7 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.xcontent.XContentType;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
